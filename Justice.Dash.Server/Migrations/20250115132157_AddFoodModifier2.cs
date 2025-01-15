316404f792d47f6aa266e979c1ba426a5c0bc240
Jeppe Beier <jeppevinkel@gmail.com>
2025-01-15 14:22:46 +0100

    Fix migrations

---
+++ Justice.Dash.Server/Migrations/20250115132157_AddFoodModifier2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Justice.Dash.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFoodModifier2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FoodModifier",
+                table: "menu_items",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FoodModifier",
+                table: "menu_items");
+        }
+    }
+}
